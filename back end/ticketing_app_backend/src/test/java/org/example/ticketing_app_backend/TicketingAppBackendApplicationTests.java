194f4bd89e5c89ee4c52c967b77ef1f8d4d0ec6d
Yucong He <hyc018018@gmail.com>
2026-08-29 19:56:24 +1000

    initial commit

---
+++ back end/ticketing_app_backend/src/test/java/org/example/ticketing_app_backend/TicketingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ticketing_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
